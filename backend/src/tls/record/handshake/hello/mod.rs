1eb42a6da1dce1bb9a89097818b9f5794e324e62
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-08-27 23:11:45 +0200

    moved extension to sep. module
    moved the extension.rs file to a separate rust module

--- backend/src/tls/record/handshake/hello/mod.rs
+++ backend/src/tls/record/handshake/hello/mod.rs
@@ -1,5 +1,5 @@
 pub use client_hello::ClientHello;
-pub use extension::{Extension, ExtensionType};
+pub use extensions::extension::{Extension, ExtensionType};
 pub use hello_request::HelloRequest;
 pub use server_hello::ServerHello;
 pub use server_hello_done::ServerHelloDone;
@@ -7,9 +7,9 @@ pub use session_id::SessionID;
 
 
 mod client_hello;
-mod extension;
 mod hello_request;
 mod server_hello;
 mod server_hello_done;
 mod session_id;
 mod signature;
+mod extensions;
