16363a0c46c8d45a0d7977760c14574cdf372f96
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-08-28 00:04:16 +0200

    allow signature algorithm to be unknown

--- backend/src/tls/record/handshake/hello/signature/signature_algorithm.rs
+++ backend/src/tls/record/handshake/hello/signature/signature_algorithm.rs
@@ -1,10 +1,22 @@
-use pwshare_macros::ReadableFromStream;
+use crate::tls::ReadableFromStream;
+use pwshare_macros::FromRepr;
 
 #[repr(u8)]
-#[derive(ReadableFromStream, Clone, Copy, Debug)]
+#[derive(FromRepr, Clone, Copy, Debug)]
 pub enum SignatureAlgorithm {
     Anonymous = 0,
     RSA = 1,
     DSA = 2,
-    ECDSA = 3
+    ECDSA = 3,
+    Unknown = 255
+}
+
+impl ReadableFromStream for SignatureAlgorithm {
+    fn read(stream: &mut impl Iterator<Item=u8>) -> std::io::Result<Self> {
+        let u = u8::read(stream)?;
+
+        Ok(
+            Self::try_from(u).unwrap_or(Self::Unknown)
+        )
+    }
 }
