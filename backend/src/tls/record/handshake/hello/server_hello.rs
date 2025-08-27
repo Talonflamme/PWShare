67e5b851123469a075070fa25202eb2b6d8492d6
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-08-27 23:02:59 +0200

    server hello struct

--- backend/src/tls/record/handshake/hello/server_hello.rs
+++ backend/src/tls/record/handshake/hello/server_hello.rs
@@ -1,4 +1,17 @@
 use pwshare_macros::ReadableFromStream;
+use crate::tls::record::cipher_suite::CipherSuite;
+use crate::tls::record::compression_method::CompressionMethod;
+use crate::tls::record::handshake::hello::{Extension, SessionID};
+use crate::tls::record::protocol_version::ProtocolVersion;
+use crate::tls::record::Random;
+use crate::tls::record::variable_length_vec::VariableLengthVec;
 
 #[derive(Debug, ReadableFromStream)]
-pub struct ServerHello;
+pub struct ServerHello {
+    server_version: ProtocolVersion,
+    random: Random,
+    session_id: SessionID,
+    cipher_suite: CipherSuite,
+    compression_method: CompressionMethod,
+    extensions: VariableLengthVec<Extension, 0, 65335>
+}
