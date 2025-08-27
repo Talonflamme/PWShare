7ec05f86bec5a430d81a487e2c6a230c6ac0e604
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-08-28 00:07:25 +0200

    allow hash algorithm to be unknown

--- backend/src/tls/record/handshake/hello/signature/hash_algorithm.rs
+++ backend/src/tls/record/handshake/hello/signature/hash_algorithm.rs
@@ -1,7 +1,8 @@
-use pwshare_macros::ReadableFromStream;
+use crate::tls::ReadableFromStream;
+use pwshare_macros::FromRepr;
 
 #[repr(u8)]
-#[derive(ReadableFromStream, Clone, Copy, Debug)]
+#[derive(FromRepr, Clone, Copy, Debug)]
 pub enum HashAlgorithm {
     None = 0,
     Md5 = 1,
@@ -9,5 +10,14 @@ pub enum HashAlgorithm {
     Sha224 = 3,
     Sha256 = 4,
     Sha384 = 5,
-    Sha512 = 6
+    Sha512 = 6,
+    Unknown = 255,
+}
+
+impl ReadableFromStream for HashAlgorithm {
+    fn read(stream: &mut impl Iterator<Item = u8>) -> std::io::Result<Self> {
+        let u = u8::read(stream)?;
+
+        Ok(Self::try_from(u).unwrap_or(Self::Unknown))
+    }
 }
--- backend/src/tls/record/handshake/hello/signature/signature_algorithm.rs
+++ backend/src/tls/record/handshake/hello/signature/signature_algorithm.rs
@@ -8,15 +8,13 @@ pub enum SignatureAlgorithm {
     RSA = 1,
     DSA = 2,
     ECDSA = 3,
-    Unknown = 255
+    Unknown = 255,
 }
 
 impl ReadableFromStream for SignatureAlgorithm {
-    fn read(stream: &mut impl Iterator<Item=u8>) -> std::io::Result<Self> {
+    fn read(stream: &mut impl Iterator<Item = u8>) -> std::io::Result<Self> {
         let u = u8::read(stream)?;
 
-        Ok(
-            Self::try_from(u).unwrap_or(Self::Unknown)
-        )
+        Ok(Self::try_from(u).unwrap_or(Self::Unknown))
     }
 }
