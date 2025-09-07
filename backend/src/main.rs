a24e827b9a303ef63f91856ccaee71ff2def847d
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-09-08 00:52:56 +0200

    old main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,9 @@
-use crate::cryptography::pem::ToPemContent;
-use crate::cryptography::rsa;
-use std::fs;
+use crate::tls::tls_main;
 
 mod cryptography;
 mod util;
 mod tls;
 
 pub fn main() {
-    // tls_main::start_server().unwrap();
-    let (pub_key, prv_key) = rsa::generate_key!(3072);
-
-    println!("{:#?}", prv_key);
-
-    let pem = prv_key.to_pem_content();
-    fs::write("private_key2.pem", pem.clone()).unwrap();
+    tls_main::start_server().unwrap();
 }
