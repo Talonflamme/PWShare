d5fa796826298f11ae9513ae79adc5c47175ad94
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-09-13 18:15:08 +0200

    implement md5
    Implemented MD5.
    Changed order of tests and added MD5 tests.
    Reworked copy_chunks_into_words macro to be more flexible and clearer.
    Fixed capacity of output vectors of multiple Sha-2 algorithms.

--- backend/src/cryptography/oaep/hashing.rs
+++ backend/src/cryptography/oaep/hashing.rs
@@ -12,30 +12,123 @@ pub trait HashFunction {
     fn hash(&self, message: &[u8]) -> Vec<u8>;
 }
 
-macro_rules! copy_chunk_into_words_u32 {
-    ($chunk:expr, $words:expr) => {
-        for i in 0..16 {
-            $words[i] = (($chunk[i * 4] as u32) << 24)
-                | (($chunk[i * 4 + 1] as u32) << 16)
-                | (($chunk[i * 4 + 2] as u32) << 8)
-                | ($chunk[i * 4 + 3] as u32);
+macro_rules! copy_chunk_into_words_be {
+    ($bytes:ident, $words:ident, $ty: ident) => {
+        for (i, chunk) in $bytes.chunks_exact(($ty::BITS / 8) as usize).enumerate() {
+            $words[i] = $ty::from_be_bytes(chunk.try_into().unwrap());
         }
     };
 }
 
-macro_rules! copy_chunk_into_words_u64 {
-    ($chunk:expr, $words:expr) => {
-        for i in 0..16 {
-            $words[i] = (($chunk[i * 8] as u64) << 56)
-                | (($chunk[i * 8 + 1] as u64) << 48)
-                | (($chunk[i * 8 + 2] as u64) << 40)
-                | (($chunk[i * 8 + 3] as u64) << 32)
-                | (($chunk[i * 8 + 4] as u64) << 24)
-                | (($chunk[i * 8 + 5] as u64) << 16)
-                | (($chunk[i * 8 + 6] as u64) << 8)
-                | ($chunk[i * 8 + 7] as u64);
+macro_rules! copy_chunk_into_words_le {
+    ($bytes:ident, $words:ident, $ty: ident) => {
+        for (i, chunk) in $bytes.chunks_exact(($ty::BITS / 8) as usize).enumerate() {
+            $words[i] = $ty::from_le_bytes(chunk.try_into().unwrap());
         }
     };
+}
+
+pub struct Md5;
+
+const K_MD5: [u32; 64] = [
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
+];
+
+impl HashFunction for Md5 {
+    fn h_len(&self) -> u32 {
+        16
+    }
+
+    fn block_size_bytes(&self) -> u32 {
+        64
+    }
+
+    fn hash(&self, message: &[u8]) -> Vec<u8> {
+        const S: [u32; 64] = [
+            7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, /**/
+            5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, /**/
+            4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, /**/
+            6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21,
+        ];
+
+        let mut a0: u32 = 0x67452301;
+        let mut b0: u32 = 0xefcdab89;
+        let mut c0: u32 = 0x98badcfe;
+        let mut d0: u32 = 0x10325476;
+
+        let mut message = message.to_vec();
+        // Pre-Processing
+        let message_len_bits = (message.len() * 8) as u64;
+        message.push(0x80); // append bit '1'
+
+        // append K '0' bits such that (L + 8 + K + 64) is a multiple of 512
+        while message.len() % 64 != 56 {
+            message.push(0);
+        }
+
+        // append the length as an u64
+        message.extend_from_slice(&message_len_bits.to_le_bytes());
+
+        for chunk in message.chunks(64) {
+            let mut a = a0;
+            let mut b = b0;
+            let mut c = c0;
+            let mut d = d0;
+
+            let mut m: [u32; 64] = [0; 64];
+
+            copy_chunk_into_words_le!(chunk, m, u32);
+
+            for i in 0..64 {
+                let mut f: u32;
+                let g: u32;
+
+                if i < 16 {
+                    f = (b & c) | ((!b) & d);
+                    g = i;
+                } else if i < 32 {
+                    f = (d & b) | ((!d) & c);
+                    g = (5 * i + 1) % 16;
+                } else if i < 48 {
+                    f = b ^ c ^ d;
+                    g = (3 * i + 5) % 16;
+                } else {
+                    f = c ^ (b | (!d));
+                    g = (7 * i) % 16;
+                }
+
+                f = f
+                    .wrapping_add(a)
+                    .wrapping_add(K_MD5[i as usize])
+                    .wrapping_add(m[g as usize]);
+                a = d;
+                d = c;
+                c = b;
+                b = b.wrapping_add(f.rotate_left(S[i as usize]));
+            }
+
+            a0 = a0.wrapping_add(a);
+            b0 = b0.wrapping_add(b);
+            c0 = c0.wrapping_add(c);
+            d0 = d0.wrapping_add(d);
+        }
+
+        let mut res = Vec::with_capacity(self.h_len() as usize);
+
+        res.extend_from_slice(&a0.to_le_bytes());
+        res.extend_from_slice(&b0.to_le_bytes());
+        res.extend_from_slice(&c0.to_le_bytes());
+        res.extend_from_slice(&d0.to_le_bytes());
+
+        res
+    }
 }
 
 pub struct Sha1;
@@ -77,7 +170,7 @@ impl HashFunction for Sha1 {
             // break chunk into 16 32-bit (=4 byte) big-endian words
             let mut w = [0u32; 80];
 
-            copy_chunk_into_words_u32!(chunk, w);
+            copy_chunk_into_words_be!(chunk, w, u32);
 
             for i in 16..80 {
                 let xor_result = w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16];
@@ -136,7 +229,7 @@ impl HashFunction for Sha1 {
             h4 = h4.wrapping_add(e);
         }
 
-        let mut result = Vec::with_capacity(20);
+        let mut result = Vec::with_capacity(self.h_len() as usize);
 
         result.extend_from_slice(&h0.to_be_bytes());
         result.extend_from_slice(&h1.to_be_bytes());
@@ -247,7 +340,7 @@ macro_rules! sha256 {
         for chunk in $message.chunks_exact(64) {
             let mut w: [u32; 64] = [0; 64];
 
-            copy_chunk_into_words_u32!(chunk, w);
+            copy_chunk_into_words_be!(chunk, w, u32);
 
             for i in 16..64 {
                 let s0 =
@@ -310,7 +403,7 @@ macro_rules! sha512 {
         for chunk in $message.chunks_exact(128) {
             let mut w: [u64; 80] = [0; 80];
 
-            copy_chunk_into_words_u64!(chunk, w);
+            copy_chunk_into_words_be!(chunk, w, u64);
 
             for i in 16..80 {
                 let s0 =
@@ -406,7 +499,7 @@ impl HashFunction for Sha256 {
 
         sha256!(message, h0, h1, h2, h3, h4, h5, h6, h7);
 
-        let mut result = Vec::with_capacity(64);
+        let mut result = Vec::with_capacity(self.h_len() as usize);
 
         result.extend_from_slice(&h0.to_be_bytes());
         result.extend_from_slice(&h1.to_be_bytes());
@@ -457,7 +550,7 @@ impl HashFunction for Sha224 {
 
         sha256!(message, h0, h1, h2, h3, h4, h5, h6, h7);
 
-        let mut result = Vec::with_capacity(64);
+        let mut result = Vec::with_capacity(self.h_len() as usize);
 
         result.extend_from_slice(&h0.to_be_bytes());
         result.extend_from_slice(&h1.to_be_bytes());
@@ -509,7 +602,7 @@ impl HashFunction for Sha512 {
 
         sha512!(message, h0, h1, h2, h3, h4, h5, h6, h7);
 
-        let mut result = Vec::with_capacity(64);
+        let mut result = Vec::with_capacity(self.h_len() as usize);
 
         result.extend_from_slice(&h0.to_be_bytes());
         result.extend_from_slice(&h1.to_be_bytes());
@@ -561,7 +654,7 @@ impl HashFunction for Sha384 {
 
         sha512!(message, h0, h1, h2, h3, h4, h5, h6, h7);
 
-        let mut result = Vec::with_capacity(48);
+        let mut result = Vec::with_capacity(self.h_len() as usize);
 
         result.extend_from_slice(&h0.to_be_bytes());
         result.extend_from_slice(&h1.to_be_bytes());
@@ -584,6 +677,24 @@ mod tests {
     }
 
     #[test]
+    fn md5() {
+        assert_eq!(
+            hash(Md5, "The quick brown fox jumps over the lazy dog"),
+            "9e107d9d372bb6826bd81d3542a419d6"
+        );
+
+        assert_eq!(
+            hash(Md5, "The quick brown fox jumps over the lazy dog."),
+            "e4d909c290d0fb1ca068ffaddf22cbd0"
+        );
+
+        assert_eq!(hash(Md5, ""), "d41d8cd98f00b204e9800998ecf8427e");
+
+        assert_eq!(Md5.h_len() * 8, 128);
+        assert_eq!(Md5.block_size_bytes() * 8, 512);
+    }
+
+    #[test]
     fn sha1() {
         assert_eq!(
             hash(Sha1, "The quick brown fox jumps over the lazy dog"),
@@ -599,27 +710,6 @@ mod tests {
 
         assert_eq!(Sha1.h_len() * 8, 160);
         assert_eq!(Sha1.block_size_bytes() * 8, 512);
-    }
-
-    #[test]
-    fn sha256() {
-        assert_eq!(
-            hash(Sha256, ""),
-            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
-        );
-
-        assert_eq!(
-            hash(Sha256, "abc"),
-            "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
-        );
-
-        assert_eq!(
-            hash(Sha256, "The quick brown fox jumps over the lazy dog"),
-            "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592"
-        );
-
-        assert_eq!(Sha256.h_len() * 8, 256);
-        assert_eq!(Sha256.block_size_bytes() * 8, 512);
     }
 
     #[test]
@@ -644,19 +734,24 @@ mod tests {
     }
 
     #[test]
-    fn sha512() {
+    fn sha256() {
         assert_eq!(
-            hash(Sha512, ""),
-            "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
+            hash(Sha256, ""),
+            "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
         );
 
         assert_eq!(
-            hash(Sha512, "The quick brown fox jumps over the lazy dog"),
-            "07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6"
+            hash(Sha256, "abc"),
+            "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad"
         );
 
-        assert_eq!(Sha512.h_len() * 8, 512);
-        assert_eq!(Sha512.block_size_bytes() * 8, 1024);
+        assert_eq!(
+            hash(Sha256, "The quick brown fox jumps over the lazy dog"),
+            "d7a8fbb307d7809469ca9abcb0082e4f8d5651e46d3cdb762d02d0bf37c9e592"
+        );
+
+        assert_eq!(Sha256.h_len() * 8, 256);
+        assert_eq!(Sha256.block_size_bytes() * 8, 512);
     }
 
     #[test]
@@ -673,5 +768,21 @@ mod tests {
 
         assert_eq!(Sha384.h_len() * 8, 384);
         assert_eq!(Sha384.block_size_bytes() * 8, 1024);
+    }
+
+    #[test]
+    fn sha512() {
+        assert_eq!(
+            hash(Sha512, ""),
+            "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e"
+        );
+
+        assert_eq!(
+            hash(Sha512, "The quick brown fox jumps over the lazy dog"),
+            "07e547d9586f6a73f73fbac0435ed76951218fb7d0c8d788a309d785436bbb642e93a252a954f23912547d1e8a3b5ed6e1bfd7097821233fa0538f3db854fee6"
+        );
+
+        assert_eq!(Sha512.h_len() * 8, 512);
+        assert_eq!(Sha512.block_size_bytes() * 8, 1024);
     }
 }
