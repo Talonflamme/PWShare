3bbef99feb6a5788ffd2049921454063174e5d71
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-09-14 04:11:47 +0200

    generation of MD5 and SHA256 constants with macros

--- backend/pwshare-macros/src/lib.rs
+++ backend/pwshare-macros/src/lib.rs
@@ -1,14 +1,16 @@
-mod readable_from_stream;
 mod from_repr;
 mod into_repr;
+mod readable_from_stream;
 mod writable_to_sink;
 
 use crate::from_repr::impl_from_repr;
 use crate::into_repr::impl_into_repr;
-use proc_macro::TokenStream;
-use readable_from_stream::impl_readable_from_stream_trait;
-use syn::{DeriveInput, Ident};
 use crate::writable_to_sink::impl_writable_to_sink;
+use proc_macro::TokenStream;
+use proc_macro2::Span;
+use quote::quote;
+use readable_from_stream::impl_readable_from_stream_trait;
+use syn::{DeriveInput, Ident, LitInt};
 
 fn get_repr_type(ast: &DeriveInput) -> Option<Ident> {
     for attr in &ast.attrs {
@@ -60,4 +62,73 @@ pub fn from_repr(item: TokenStream) -> TokenStream {
 pub fn into_repr(item: TokenStream) -> TokenStream {
     let ast = syn::parse(item).unwrap();
     impl_into_repr(ast)
+}
+
+#[proc_macro]
+pub fn generate_k_md5(_: TokenStream) -> TokenStream {
+    let md5: Vec<_> = (0..64)
+        .map(|i| (f64::sin(i as f64 + 1.0).abs() * (1u64 << 32) as f64) as u32)
+        .map(|n| LitInt::new(format!("0x{:x}u32", n).as_str(), Span::call_site()))
+        .collect();
+    quote! {
+        [#(#md5),*]
+    }
+    .into()
+}
+
+fn generate_primes(n: usize) -> Box<dyn Iterator<Item = u32>> {
+    Box::new(
+        (2..)
+            .filter(|x| {
+                // is prime?
+                if *x <= 3 {
+                    return true;
+                }
+
+                for i in 2..=((*x as f32).sqrt().ceil() as u32) {
+                    if x % i == 0 {
+                        // composite
+                        return false;
+                    }
+                }
+                true
+            })
+            .take(n),
+    )
+}
+
+#[proc_macro]
+pub fn generate_k_sha256(_: TokenStream) -> TokenStream {
+    // first 64 primes
+    let primes = generate_primes(64);
+
+    let fractional_parts = primes.map(|prime| {
+        let cbrt = (prime as f64).cbrt();
+        let frac = (cbrt.fract() + 1.0).to_bits();
+
+        // by only taking the `.fract()` and then adding 1, the mantissa/fractional/significand
+        // becomes exactly the part we need
+
+        // mantissa is 52 bits, we want 32 bits of precision -> shift by 20
+        ((frac) >> ((f64::MANTISSA_DIGITS - 1) - 32)) as u32
+    });
+
+    let literals = fractional_parts
+        .map(|frac| LitInt::new(format!("0x{:x}u32", frac).as_str(), Span::call_site()));
+
+    quote! {
+        [#(#literals),*]
+    }
+    .into()
+}
+
+#[proc_macro]
+pub fn generate_k_sha512(_: TokenStream) -> TokenStream {
+    let primes = generate_primes(80);
+
+    let fractionals = primes.map(|prime| {
+        let cbrt = ();
+    });
+
+    quote! {}.into()
 }
--- backend/src/cryptography/oaep/hashing.rs
+++ backend/src/cryptography/oaep/hashing.rs
@@ -1,3 +1,5 @@
+use pwshare_macros::{generate_k_md5, generate_k_sha256};
+
 /// A trait that is implemented for a couple of HashFunctions.
 pub trait HashFunction {
     // This function and `.block_size_bytes(&self)` are functions as the trait cannot be wrapped
@@ -30,16 +32,7 @@ macro_rules! copy_chunk_into_words_le {
 
 pub struct Md5;
 
-const K_MD5: [u32; 64] = [
-    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
-];
+const K_MD5: [u32; 64] = generate_k_md5!();
 
 impl HashFunction for Md5 {
     fn h_len(&self) -> u32 {
@@ -241,16 +234,7 @@ impl HashFunction for Sha1 {
     }
 }
 
-const K_256: [u32; 64] = [
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
-];
+const K_256: [u32; 64] = generate_k_sha256!();
 
 const K_512: [u64; 80] = [
     0x428a2f98d728ae22,
@@ -674,6 +658,35 @@ mod tests {
 
     fn hash(h: impl HashFunction, message: &str) -> String {
         h.hash(message.as_bytes()).hex()
+    }
+
+    #[test]
+    fn const_md5() {
+        let k_md5 = generate_k_md5!();
+        // just do a couple of tests
+        assert_eq!(k_md5.len(), 64);
+        assert_eq!(k_md5[0], 0xd76aa478);
+        assert_eq!(k_md5[10], 0xffff5bb1);
+        assert_eq!(k_md5[40], 0x289b7ec6);
+        assert_eq!(k_md5[50], 0xab9423a7);
+        assert_eq!(k_md5[63], 0xeb86d391);
+    }
+
+    #[test]
+    fn const_sha256() {
+        let k_sha256 = generate_k_sha256!();
+
+        assert_eq!(k_sha256.len(), 64);
+        assert_eq!(k_sha256[0], 0x428a2f98);
+        assert_eq!(k_sha256[10], 0x243185be);
+        assert_eq!(k_sha256[31], 0x14292967);
+        assert_eq!(k_sha256[50], 0x2748774c);
+        assert_eq!(k_sha256[63], 0xc67178f2);
+    }
+
+    #[test]
+    fn const_sha512() {
+
     }
 
     #[test]
