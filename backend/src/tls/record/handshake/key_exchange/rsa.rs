3aca0b21cdd60a1c3f80cc2f3ae07799d70e0677
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-09-16 03:22:10 +0200

    oops, small import error

--- backend/src/tls/record/handshake/key_exchange/rsa.rs
+++ backend/src/tls/record/handshake/key_exchange/rsa.rs
@@ -2,7 +2,7 @@ use crate::tls::record::public_key_encrypted::PublicKeyEncrypted;
 use crate::util::UintDisplay;
 use pwshare_macros::{ReadableFromStream, WritableToSink};
 use std::fmt::{Debug, Formatter};
-use crate::tls::connection_state::security_parameters::PRFAlgorithm;
+use crate::tls::connection_state::prf::PRFAlgorithm;
 use crate::tls::record::protocol_version::ProtocolVersion;
 
 #[derive(Debug, ReadableFromStream, WritableToSink)]
