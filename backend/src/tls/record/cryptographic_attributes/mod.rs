dc7bc3d154541bff22e14f5c70bf426e1ef370dc
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-10-13 15:05:28 +0200

    change attributes to use macro impl

--- backend/src/tls/record/cryptographic_attributes/mod.rs
+++ backend/src/tls/record/cryptographic_attributes/mod.rs
@@ -1,7 +1,24 @@
 mod public_key_encrypted;
-mod stream_ciphered;
-mod block_ciphered;
+mod aead_ciphered;
 
+use std::marker::PhantomData;
 pub use public_key_encrypted::PublicKeyEncrypted;
-pub use stream_ciphered::StreamCiphered;
-pub use block_ciphered::BlockCiphered;
+
+macro_rules! impl_ciphered {
+    ($typ: ident) => {
+        pub struct $typ <T> {
+            pub bytes: Vec<u8>,
+            _marker: PhantomData<T>
+        }
+
+        impl<T> $typ <T> {
+            pub fn new(bytes: Vec<u8>) -> Self {
+                Self { bytes, _marker: PhantomData }
+            }
+        }
+    }
+}
+
+impl_ciphered!(BlockCiphered);
+impl_ciphered!(StreamCiphered);
+impl_ciphered!(AeadCiphered);
