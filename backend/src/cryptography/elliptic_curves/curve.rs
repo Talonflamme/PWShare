9c76f0a90b1280291a30c9d090280d4e16e9d9c3
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2025-10-18 21:06:06 +0200

    refactor EllipticCurveSpecification to EllipticCurve

--- backend/src/cryptography/elliptic_curves/curve.rs
+++ backend/src/cryptography/elliptic_curves/curve.rs
@@ -1,4 +1,27 @@
-#[derive(Debug, Clone, Copy)]
+use num_bigint::BigUint;
+
+#[derive(Debug, Clone)]
+pub struct ECPoint {
+    pub x: BigUint,
+    pub y: BigUint,
+}
+
+#[allow(non_snake_case)]
+#[derive(Debug, Clone)]
 pub struct EllipticCurve {
-    
+    /// How many bytes each coordinate is encoded in. This is the same as
+    /// `ceil(PrimeFieldSize / 8)`. For `X25519`, this is for example equal
+    /// to `ceil(255 / 8) = 32`.
+    pub coordinate_length: usize,
+    /// The base point `G`, also Generator Point.
+    pub G: ECPoint,
+    /// Curve constant `a` as in `y² = x³ + ax + b (mod p)`.
+    pub a: BigUint,
+    /// Curve constant `b` as in `y² = x³ + ax + b (mod p)`. This might be unused for
+    /// Montgomery curves like X25519 and X448.
+    pub b: BigUint,
+    /// Base point order `n`.
+    pub n: BigUint,
+    /// The Prime `p`.
+    pub p: BigUint,
 }
--- backend/src/tls/record/handshake/key_exchange/ecdhe/tests.rs
+++ backend/src/tls/record/handshake/key_exchange/ecdhe/tests.rs
@@ -6,7 +6,6 @@ use num_traits::Num;
 fn test_x25519_params() {
     let curve = NamedCurve::X25519.curve().unwrap();
 
-    assert_eq!(curve.name, NamedCurve::X25519);
     assert_eq!(curve.coordinate_length, 32);
     assert_eq!(curve.p, BigUint::from(2u8).pow(255) - BigUint::from(19u8));
     assert_eq!(
@@ -15,6 +14,5 @@ fn test_x25519_params() {
             + BigUint::from_str_radix("14def9dea2f79cd65812631a5cf5d3ed", 16).unwrap()
     );
     assert_eq!(curve.a, BigUint::from(486662u32));
-    assert_eq!(curve.G.point.form, PointConversionForm::Uncompressed);
-    assert_eq!(curve.G.point.x, BigUint::from(9u32));
+    assert_eq!(curve.G.x, BigUint::from(9u32));
 }
