fda3c6dde48161e0e4188b3de70084c2ba8b9dd8
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2026-06-03 20:35:44 +0200

    tests for X448 params

--- backend/src/tls/record/handshake/key_exchange/ecdhe/tests.rs
+++ backend/src/tls/record/handshake/key_exchange/ecdhe/tests.rs
@@ -2,7 +2,7 @@ use crate::cryptography::elliptic_curves::curve::*;
 use crate::tls::record::key_exchange::ecdhe::elliptic_curve::NamedCurve;
 use crate::util::bytes_from_hex;
 use num_bigint::BigUint;
-use num_traits::Num;
+use num_traits::{Num, One};
 use std::str::FromStr;
 
 #[test]
@@ -26,17 +26,54 @@ fn test_x25519_params() {
 }
 
 #[test]
+fn test_x448_params() {
+    let curve = NamedCurve::X448.curve().unwrap();
+
+    let two = BigUint::from(2u8);
+
+    assert_eq!(curve.coordinate_length, 56);
+    assert_eq!(curve.p, two.pow(448) - two.pow(224) - BigUint::one());
+    assert_eq!(
+        curve.n,
+        two.pow(446)
+            - BigUint::from_str_radix(
+                "8335dc163bb124b65129c96fde933d8d723a70aadc873d6d54a7bb0d",
+                16
+            )
+            .unwrap()
+    );
+    assert_eq!(curve.G.x, BigUint::from(5_u32));
+
+    if let EllipticCurveConstants::Montgomery { A } = &curve.constants {
+        assert_eq!(A, &BigUint::from(0x262a6_u32));
+    } else {
+        assert!(false, "X448 should be in Montgomery form");
+    }
+}
+
+#[test]
 fn test_x25519_scalar_multiply() {
     let curve = NamedCurve::X25519.curve().unwrap();
 
-    let scalar = BigUint::from_str("31029842492115040904895560451863089656472772604678260265531221036453811406496").unwrap();
-    let u = BigUint::from_str("34426434033919594451155107781188821651316167215306631574996226621102155684838").unwrap();
+    let scalar = BigUint::from_str(
+        "31029842492115040904895560451863089656472772604678260265531221036453811406496",
+    )
+    .unwrap();
+    let u = BigUint::from_str(
+        "34426434033919594451155107781188821651316167215306631574996226621102155684838",
+    )
+    .unwrap();
 
-    let point = Point { x: u, y: BigUint::ZERO };
+    let point = Point {
+        x: u,
+        y: BigUint::ZERO,
+    };
 
     let output = curve.scalar_multiply(&scalar, point).x;
 
-    let expected_output = BigUint::from_bytes_le(&bytes_from_hex("c3da55379de9c6908e94ea4df28d084f32eccf03491c71f754b4075577a28552"));
+    let expected_output = BigUint::from_bytes_le(&bytes_from_hex(
+        "c3da55379de9c6908e94ea4df28d084f32eccf03491c71f754b4075577a28552",
+    ));
 
     assert_eq!(output, expected_output);
 }
