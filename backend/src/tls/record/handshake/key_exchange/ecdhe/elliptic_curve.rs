83c3c589504f8106acf6d2785a14879606ed0aa2
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2026-06-03 20:29:08 +0200

    corrected limbs for p and n for X448
    Limbs were missing the most significant digit. Test now passes.

--- backend/src/tls/record/handshake/key_exchange/ecdhe/elliptic_curve.rs
+++ backend/src/tls/record/handshake/key_exchange/ecdhe/elliptic_curve.rs
@@ -233,7 +233,7 @@ impl NamedCurve {
                 p: BigUint::new(vec![
                     0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
                     0xffffffff, 0xfffffffe, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-                    0xffffffff,
+                    0xffffffff, 0xffffffff,
                 ]), // 2^448 - 2^224 - 1
                 constants: EllipticCurveConstants::Montgomery {
                     A: BigUint::from(156326_u32),
@@ -241,7 +241,7 @@ impl NamedCurve {
                 n: BigUint::new(vec![
                     0xab5844f3, 0x2378c292, 0x8dc58f55, 0x216cc272, 0xaed63690, 0xc44edb49,
                     0x7cca23e9, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff,
-                    0xffffffff,
+                    0xffffffff, 0x3fffffff,
                 ]), // 2^446 - 0x8335dc163bb124b65129c96fde933d8d723a70aadc873d6d54a7bb0d
                 G: Point {
                     x: BigUint::from(5_u32),
