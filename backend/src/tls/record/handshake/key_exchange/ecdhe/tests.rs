e79884326f2e6a18e6508497f6c3789d567cf458
Talonflamme <94370168+Talonflamme@users.noreply.github.com>
2026-06-10 17:27:30 +0200

    add tests for NIST curves
    Tests added for the NIST curves: SECP256R1, SECP384R1, SECP521R1. Every test passes. Issue with SECP521R1 not yet fixed.

--- backend/src/tls/record/handshake/key_exchange/ecdhe/tests.rs
+++ backend/src/tls/record/handshake/key_exchange/ecdhe/tests.rs
@@ -1,6 +1,6 @@
 use crate::cryptography::elliptic_curves::curve::*;
 use crate::tls::record::key_exchange::ecdhe::elliptic_curve::NamedCurve;
-use crate::util::bytes_from_hex;
+use crate::util::{bytes_from_hex, UintDisplay};
 use crypto_bigint::BoxedUint;
 
 #[inline]
@@ -76,6 +76,66 @@ fn test_x448_params() {
         );
     } else {
         panic!("X448 should be in Montgomery form");
+    }
+}
+
+#[test]
+fn test_secp256r1_params() {
+    let curve = NamedCurve::SECP256R1.curve().unwrap();
+
+    // 256-bits, 32 bytes
+    assert_eq!(curve.coordinate_length, 32);
+
+    assert_eq!(curve.p.hex(), "ffffffff00000001000000000000000000000000ffffffffffffffffffffffff");
+    assert_eq!(curve.n.hex(), "ffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551");
+    assert_eq!(curve.G.x.hex(), "6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296");
+    assert_eq!(curve.G.y.hex(), "4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5");
+
+    if let EllipticCurveConstants::Weierstrass { a, b } = &curve.constants {
+        assert_eq!(a.hex(), "ffffffff00000001000000000000000000000000fffffffffffffffffffffffc");
+        assert_eq!(b.hex(), "5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b");
+    } else {
+        panic!("SECP256R1 should be in Weierstrass form");
+    }
+}
+
+#[test]
+fn test_secp384r1_params() {
+    let curve = NamedCurve::SECP384R1.curve().unwrap();
+
+    // 384-bit, 48 bytes
+    assert_eq!(curve.coordinate_length, 48);
+
+    assert_eq!(curve.p.hex(), "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000ffffffff");
+    assert_eq!(curve.n.hex(), "ffffffffffffffffffffffffffffffffffffffffffffffffc7634d81f4372ddf581a0db248b0a77aecec196accc52973");
+    assert_eq!(curve.G.x.hex(), "aa87ca22be8b05378eb1c71ef320ad746e1d3b628ba79b9859f741e082542a385502f25dbf55296c3a545e3872760ab7");
+    assert_eq!(curve.G.y.hex(), "3617de4a96262c6f5d9e98bf9292dc29f8f41dbd289a147ce9da3113b5f0b8c00a60b1ce1d7e819d7a431d7c90ea0e5f");
+
+    if let EllipticCurveConstants::Weierstrass { a, b } = &curve.constants {
+        assert_eq!(a.hex(), "fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffeffffffff0000000000000000fffffffc");
+        assert_eq!(b.hex(), "b3312fa7e23ee7e4988e056be3f82d19181d9c6efe8141120314088f5013875ac656398d8a2ed19d2a85c8edd3ec2aef");
+    } else {
+        panic!("SECP384R1 should be in Weierstrass form");
+    }
+}
+
+#[test]
+fn test_secp521r1_params() {
+    let curve = NamedCurve::SECP521R1.curve().unwrap();
+
+    // 521-bit, 66 bytes
+    assert_eq!(curve.coordinate_length, 66);
+
+    assert_eq!(curve.p.hex(), "1ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+    assert_eq!(curve.n.hex(), "1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffa51868783bf2f966b7fcc0148f709a5d03bb5c9b8899c47aebb6fb71e91386409");
+    assert_eq!(curve.G.x.hex(), "c6858e06b70404e9cd9e3ecb662395b4429c648139053fb521f828af606b4d3dbaa14b5e77efe75928fe1dc127a2ffa8de3348b3c1856a429bf97e7e31c2e5bd66");
+    assert_eq!(curve.G.y.hex(), "11839296a789a3bc0045c8a5fb42c7d1bd998f54449579b446817afbd17273e662c97ee72995ef42640c550b9013fad0761353c7086a272c24088be94769fd16650");
+
+    if let EllipticCurveConstants::Weierstrass { a, b } = &curve.constants {
+        assert_eq!(a.hex(), "1fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffc");
+        assert_eq!(b.hex(), "51953eb9618e1c9a1f929a21a0b68540eea2da725b99b315f3b8b489918ef109e156193951ec7e937b1652c0bd3bb1bf073573df883d2c34f1ef451fd46b503f00");
+    } else {
+        panic!("SECP521R1 should be in Weierstrass form");
     }
 }
 
